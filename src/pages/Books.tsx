15c192f03dada2da2d8b5c47a56e70d3d84fe22f
robiulalam76 <robiulalam762400@gmail.com>
2023-07-16 14:35:26 +0600

    book page search and filter design added

--- src/pages/Books.tsx
+++ src/pages/Books.tsx
@@ -1,17 +1,77 @@
 import ProductCard from '@/components/ProductCard';
 import { useGetBooksQuery } from '@/redux/features/products/productApi';
+import { setGenre, setPublicationYear } from '@/redux/features/products/productSlice';
+import { useAppDispatch, useAppSelector } from '@/redux/hook';
 import { IBook } from '@/types/bookTypes';
+import { Button, Menu, MenuHandler, MenuItem, MenuList, Tooltip } from '@material-tailwind/react';
+import { useState } from 'react';
 
+
+const genres = ["dfdf", "dfdf"]
+const yeares = ["dfdf", "dfdf"]
 
 export default function Books() {
   const { data, isLoading, error } = useGetBooksQuery(undefined);
+  const { genre, publicationYear } = useAppSelector((state) => state.product)
+  const dispatch = useAppDispatch()
+
+
+
   console.log(isLoading, error)
 
   return (
-    <div className='max-w-[1480px] mx-auto px-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 p-4'>
-      {
-        data?.data?.map((book: IBook, i: number) => <ProductCard key={i} data={book} />)
-      }
-    </div>
+    <section className='max-w-[1440px] mx-auto px-4 '>
+      <div className='flex flex-col md:flex-row md:items-center justify-end gap-2 md:gap-8 mt-12 md:p-4'>
+        <div className="flex items-center w-full">
+          <input
+            className="flex-grow md:flex-grow-0 h-10 w-full focus:outline-none border border-blue-600 focus:border-primary px-2"
+            type="search"
+            name="search"
+            placeholder="What do you need?"
+          />
+          <Tooltip content="Search">
+            <button
+              className="w-36 h-10 text-white bg-blue-600 hover:bg-blue-700 duration-150 flex justify-center items-center"
+              type="submit"
+            >
+              <span>Search</span>
+            </button>
+          </Tooltip>
+        </div>
+
+        <div className='md:w-[400px] grid grid-cols-2 gap-4'>
+          <Tooltip content="Genre" >
+            <Menu>
+              <MenuHandler>
+                <Button className='rounded-sm'>{genre ? genre : "Genre"}</Button>
+              </MenuHandler>
+              <MenuList>
+                {
+                  genres?.map((genre: string, i: number) => <MenuItem onClick={() => dispatch(setGenre(genre))} key={i} >{genre}</MenuItem>)
+                }
+              </MenuList>
+            </Menu>
+          </Tooltip>
+          <Tooltip content="Publication Year">
+            <Menu>
+              <MenuHandler>
+                <Button className='rounded-sm'>{publicationYear ? publicationYear : "Year"}</Button>
+              </MenuHandler>
+              <MenuList>
+                {
+                  yeares?.map((year: string, i: number) => <MenuItem onClick={() => dispatch(setPublicationYear(year))} key={i} >{year}</MenuItem>)
+                }
+              </MenuList>
+            </Menu>
+          </Tooltip>
+        </div>
+
+      </div>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 p-4'>
+        {
+          data?.data?.map((book: IBook, i: number) => <ProductCard key={i} data={book} />)
+        }
+      </div>
+    </section>
   );
 }
--- src/redux/features/products/productSlice.ts
+++ src/redux/features/products/productSlice.ts
@@ -4,11 +4,15 @@ import type { PayloadAction } from '@reduxjs/toolkit';
 interface IProduct {
   status: boolean;
   priceRange: number;
+  genre: string;
+  publicationYear: string;
 }
 
 const initialState: IProduct = {
   status: false,
   priceRange: 150,
+  genre: "",
+  publicationYear: "",
 };
 
 const productSlice = createSlice({
@@ -21,9 +25,15 @@ const productSlice = createSlice({
     setPriceRange: (state, action: PayloadAction<number>) => {
       state.priceRange = action.payload;
     },
+    setGenre: (state, action: PayloadAction<string>) => {
+      state.genre = action.payload;
+    },
+    setPublicationYear: (state, action: PayloadAction<string>) => {
+      state.publicationYear = action.payload;
+    },
   },
 });
 
-export const { toggleState, setPriceRange } = productSlice.actions;
+export const { toggleState, setPriceRange, setGenre, setPublicationYear } = productSlice.actions;
 
 export default productSlice.reducer;
